68092fa7a1f24bb0b69018b47af525a378031090
DU <sangdyjjang@naver.com>
2025-09-28 14:20:12 +0900

    Feat : 채팅 메시지 수신 시 사용자 언어에 따라 번역 기능 추가

--- index.js
+++ index.js
@@ -21,10 +21,25 @@ const ChatMessage = mongoose.model("ChatMessage", chatMessageSchema);
 
 app.use(express.static("public"));
 
+//api 연동 부분
+const translateText = async (text, targetLang) => {
+  //api 로직 구현
+  return `[Translated to ${targetLang}]: ${text}`;
+};
+
 //유저 연결 시
 io.on("connection", async (socket) => {
   let username = null;
-  console.log("유저가 연결되었습니다.");
+  let preferredLanguage = "ko"; // 기본값 한국어
+
+  socket.on("new user and lang", (data) => {
+    username = data.name;
+    preferredLanguage = data.lang;
+    io.emit("chat message", {
+      user: "시스템",
+      msg: `${username}님이 입장하셨습니다.`,
+    });
+  });
 
   //DB에서 모든 채팅 기록 불러오기
   try {
@@ -54,8 +69,20 @@ io.on("connection", async (socket) => {
     //메세지 저장
     await chatMessage.save();
 
-    //메세지 브로드캐스트
-    io.emit("chat message", data);
+    //접속 중인 소켓 순회하며 번역 후 전송
+    io.sockets.sockets.forEach(async (receiverSocket) => {
+      const receiverLang = receiverSocket.preferredLanguage || "ko";
+
+      //번역 수행
+      const translateMsg = await translateText(data.msg, receiverLang);
+
+      //번역된 메세지 전송
+      receiverSocket.emit("chat message", {
+        user: data.user,
+        msg: translateMsg,
+        originalMsg: data.msg,
+      });
+    });
   });
 
   //유저 연결 해제 시
